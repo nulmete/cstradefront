a4d42fd001171435391e6080b9a1697534c1ba3f
nicoulmete <nicolas.ulmete@mandiant.com>
2021-08-12 12:10:22 -0300

    trying master push

--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -4,6 +4,6 @@ import Home from './Home';
 
 test('Test Home.js', () => {
   const { getByText } = render(<Home />);
-  const homeText = getByText('Responsive drawer');
+  const homeText = getByText('Responsive');
   expect(homeText).toBeTruthy();
 });
