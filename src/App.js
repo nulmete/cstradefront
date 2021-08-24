3c447c8df41a48629051c5c81499d32cb23b7afc
nulmete <nicoulmete1@gmail.com>
2021-08-23 23:10:13 -0300

    remove comma

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { makeStyles } from '@material-ui/core';
-import React, { useState } from 'react';
-import CustomButton from './components/CustomButton/CustomButton';
+import { makeStyles } from "@material-ui/core";
+import React, { useState } from "react";
+import CustomButton from "./components/CustomButton/CustomButton";
 
 const useStyles = makeStyles(() => ({
   root: {
-    height: '100vh',
-    width: '100vw',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    height: "100vh",
+    width: "100vw",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   },
   buttonsContainer: {
-    '& > *:not(:last-child)': {
-      marginRight: '1rem',
+    "& > *:not(:last-child)": {
+      marginRight: "1rem",
     },
   },
 }));
